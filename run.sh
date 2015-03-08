93f9b9583d84a85ba92ab009447f462c983561c9
Michał 'rysiek' Woźniak <rysiek@fwioo.pl>
2015-03-09 00:36:32 +0100

    licencja

--- run.sh
+++ run.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+#
+# e-Dockleracje -- zdokeryzowane e-Deklaracje
+# Copyright (C) 2015 Michał "rysiek" Woźniak <rysiek@hackerspace.pl>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 # upewniamy się, że działamy jako wrapper.sh, a więc już w dockerze
 if [[ `basename $0` != 'wrapper.sh' ]]; then
@@ -14,7 +31,9 @@ chown -R "$EDEKLARACJE_UID":"$EDEKLARACJE_GID" "$EDEKLARACJE_HOME"
 
 exec su - $EDEKLARACJE_USER -c "
 # magic: http://www.linuxquestions.org/questions/linux-newbie-8/xlib-connection-to-0-0-refused-by-server-xlib-no-protocol-specified-152556/
-#xauth add \$HOSTNAME/unix:0 $MIT_COOKIE
+# na wszelki wypadek -- jeśli gid/uid/username się zgadzają, to powinno niby wszystko działać...
+xauth add \$HOSTNAME/unix:0 $MIT_COOKIE
+
 #/opt/e-Deklaracje/bin/e-Deklaracje
 /bin/bash
 "
